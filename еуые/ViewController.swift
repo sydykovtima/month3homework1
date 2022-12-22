4d1950c1c4e2e91cf65b757a79d11cbe75e61e1e
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2022-12-22 16:40:15 +0600

    Update ViewController.swift

--- еуые/ViewController.swift
+++ еуые/ViewController.swift
@@ -4,16 +4,77 @@
 //
 //  Created by Mac on 12/12/2022.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberOne: UITextField!
+    
+    @IBOutlet weak var numberTwo: UITextField!
+    
+    @IBOutlet weak var advice: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func plus(_ sender: UIButton) {
+        
+        
+        guard let num = Int(numberOne.text!) else {
+            advice.text = "Введите число!"
+            return
+        }
+        
+        guard let num1 = Int(numberTwo.text!) else {
+            advice.text = "Введите число!"
+            return
+            
+        }
+
+        advice.text = String(num + num1)
     }
+    
+    @IBAction func minus(_ sender: UIButton) {
+        
+        guard let num = Int(numberOne.text!) else {
+            advice.text = "Введите число!"
+            return
+        }
+        
+        guard let num1 = Int(numberTwo.text!) else {
+            advice.text = "Введите число!"
+            return
+            
+        }
 
+        advice.text = String(num - num1)
+    }
+    
+    @IBAction func multiply(_ sender: UIButton) {
+        
+        guard let num = Int(numberOne.text!) else {
+            advice.text = "Введите число!"
+            return
+        }
+        
+        guard let num1 = Int(numberTwo.text!) else {
+            advice.text = "Введите число!"
+            return
+            
+        }
 
-}
+        advice.text = String(num * num1)
+    }
+    
+    @IBAction func devide(_ sender: UIButton) {
+        
+        guard let num = Int(numberOne.text!) else {
+            advice.text = "Введите число!"
+            return
+        }
+        
+        guard let num1 = Int(numberTwo.text!) else {
+            advice.text = "Введите число!"
+            return
+            
+        }
 
+        advice.text = String(num / num1)
+    }
